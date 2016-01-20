029aabf50101cb21b4c6ef67bd337ed0d106b7bc
Oscar Swanros <oscar@swanros.com>
2016-01-19 18:24:37 -0600

    Create DownloadJSONByPollingOperation

---
+++ PNetworkKit/Operations/Generics/DownloadJSONByPollingOperation.swift
@@ -0,0 +1,7 @@
+
+public class DownloadJSONByPollingOperation: DownloadJSONOperation {
+    
+    public override func operationDidFinish(operation: NSOperation, withErrors errors: [NSError]) {
+        <#code#>
+    }
+}
