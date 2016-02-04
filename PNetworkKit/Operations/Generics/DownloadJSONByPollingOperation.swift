fb174b750c01afd1bef5c26677da5fd08eed8f68
Oscar Swanros <oscar@swanros.com>
2016-02-03 18:47:21 -0600

    Clean up a bit

--- PNetworkKit/Operations/Generics/DownloadJSONByPollingOperation.swift
+++ PNetworkKit/Operations/Generics/DownloadJSONByPollingOperation.swift
@@ -7,8 +7,9 @@ public class DownloadJSONByPollingOperation<T: Pollable, P: DownloadJSONOperatio
     private var hasProducedAlert = false
     private var pollToURL: NSURL?
     private var pollState: PollStatusProtocol
-    private var __completion: (T -> Void)?
-    private var __error: (NSError -> Void)?
+    public var __completion: (T -> Void)?
+    // WARNING: __completion and _error are ugly
+    public var __error: (NSError -> Void)?
     private var model: T?
     
     
@@ -39,6 +40,7 @@ public class DownloadJSONByPollingOperation<T: Pollable, P: DownloadJSONOperatio
     }
     
     
+    // WARNING: Something crashes after the response is delivered back to the caller
     // MARK: - Overrides
     public override func finish(errors: [NSError]) {
         if pollState.hasFinished() {
@@ -46,7 +48,6 @@ public class DownloadJSONByPollingOperation<T: Pollable, P: DownloadJSONOperatio
                 parsedJSON = _m
                 __completion?(_m)
             }
-            internalQueue.suspended = true
             super.finish(errors)
         } else {
             internalQueue.suspended = false
@@ -72,11 +73,14 @@ public class DownloadJSONByPollingOperation<T: Pollable, P: DownloadJSONOperatio
             }
             
             if let operation = operation as? DownloadJSONOperation where (operation.name == pollingDownloadOperation.name || operation.name == initialDownloadOperation.name) {
-                guard let json = operation.downloadedJSON else {
-                    return
+                guard
+                    let json = operation.downloadedJSON,
+                    result = json["result"] as? [String:AnyObject]
+                    else {
+                        return
                 }
                 
-                model = T.withData(json)
+                model = T.withData(result)
                 
                 guard let _m = model else {
                     return
@@ -111,6 +115,7 @@ extension DownloadJSONByPollingOperation {
             guard let clone = pollingDownloadOperation.clone() else {
                 return
             }
+            
             _do = clone
             _do?.url = pollToURL
         }
